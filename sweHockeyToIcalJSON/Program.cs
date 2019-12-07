07b5ebfd4e1df868e659fb632c2b4a8586f90025
Robothelvete <robothelvete@gmail.com>
2019-12-07 16:59:53 +0100

    added support for major leagues schedule as well

--- sweHockeyToIcalJSON/Program.cs
+++ sweHockeyToIcalJSON/Program.cs
@@ -58,35 +58,69 @@ namespace sweHockeyToIcalJSON
             return link;
         }
 
+        private static string cleanTeams(string teams)
+        {
+            return teams.Replace(System.Environment.NewLine, "").Replace("            ", "");
+        }
+        
+
         /// <summary>
         /// Parse HTML to get a list of all games it contains
         /// </summary>
         /// <param name="html"></param>
         /// <returns></returns>
-        public static List<Game> gamesFromHtml(string html)
+        public static List<Game> gamesFromHtml(string html, ParserMode mode = ParserMode.Unknown)
         {
             List<Game> games = new List<Game>();
             var svCulture = System.Globalization.CultureInfo.GetCultureInfo("sv-SE");
 
             HtmlDocument doc = new HtmlDocument();
             doc.LoadHtml(html);
-            foreach (var node in doc.DocumentNode.SelectNodes("//table[@class='tblContent table']/tr"))
+
+            string lastDate = null; //needed to keep track of dates in SDHL/HA/SDHL mode, since the date isn't given in every row
+
+            foreach (var node in doc.DocumentNode.SelectNodes("//table[contains(@class,'tblContent')]/tr"))
             {
                 if (node.ChildNodes.Any(n => n.Name == "th"))
                 {
+                    if (mode == ParserMode.Unknown && node.ChildNodes.Any(n => n.Attributes["class"].Value.Contains("tdHeader")))
+                    {
+                        //Use this to get the layout of the rest of the table
+                        if(node.ChildNodes.Last().InnerText == "Group") { mode = ParserMode.HockeyEttan; }
+                        else { mode = ParserMode.SHL; }
+                    }
                     continue;
                 }
-                string strDate = node.SelectSingleNode("td/div[@class='dateLink']/span").InnerText;
-                string teams = node.SelectSingleNode("td[3]").InnerText;
-                string location = node.SelectSingleNode("td[7]").InnerText;
-                string series = node.SelectSingleNode("td[8]").InnerText;
-                string url = node.SelectSingleNode("td/a")?.Attributes["href"].Value;
+                string strDate;
+                string teams;
+                string location;
+                string series = null; //assign null explicitly to tell compiler to STFU
+                string url;
+                if((int)mode == 1)
+                {
+                    strDate = node.SelectSingleNode("td/div[@class='dateLink']/span").InnerText;
+                    teams = cleanTeams(node.SelectSingleNode("td[3]").InnerText);
+                    location = node.SelectSingleNode("td[7]").InnerText;
+                    series = node.SelectSingleNode("td[8]").InnerText;
+                    url = node.SelectSingleNode("td/a")?.Attributes["href"].Value.Replace("&#xD;&#xA;", "");
+                } else if((int)mode == 2)
+                {
+                    //strDate = node.SelectSingleNode("td[2]").InnerText;
+                    if(!string.IsNullOrEmpty(node.SelectSingleNode("td[2]").InnerText))
+                    {
+                        lastDate = node.SelectSingleNode("td[2]/bold").InnerText + " ";
+                    }
+                    strDate = lastDate + node.SelectSingleNode("td/div[@class='dateLink']/span").InnerText;
+                    teams = cleanTeams(node.SelectSingleNode("td[4]").InnerText);
+                    location = node.SelectSingleNode("td[8]").InnerText;
+                    url = node.SelectSingleNode("td/a")?.Attributes["href"].Value.Replace("&#xD;&#xA;","");
+                }
+                else { throw new Exception("Could not figure out how to parse this HTML"); }
                 Game game = new Game()
                 {
                     Lag = teams,
                     Location = location,
-                    Tid = DateTime.ParseExact(strDate, "yyyy-MM-dd HH:mm", svCulture),
-                    Series = series
+                    Tid = DateTime.ParseExact(strDate, "yyyy-MM-dd HH:mm", svCulture)
                 };
                 game.End = game.Tid.AddHours(2); //basically just for ical compatibility anyway
                 if (!string.IsNullOrEmpty(url))
@@ -95,6 +129,11 @@ namespace sweHockeyToIcalJSON
                 }
                 else { game.Url = ""; }
                 game.Uid = "swehockey_" + getGameId(game.Url); //basically just for ical compatibility anyway
+                if(!string.IsNullOrEmpty(series))
+                {
+                    game.Series = series;
+                }
+
 
                 games.Add(game);
             }
@@ -163,5 +202,14 @@ namespace sweHockeyToIcalJSON
         }
 
 
+    }
+
+    public enum ParserMode
+    {
+        Unknown = 0,
+        HockeyEttan = 1,
+        SHL = 2,
+        HA = 2,
+        SDHL = 2
     }
 }
